a4a0a959de9ba469865f43589e4b52aea178dd53
AGravino76 <85258567+AGravino76@users.noreply.github.com>
2021-06-02 17:31:07 -0700

    Add files via upload

---
+++ GettingClose.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class GettingClose{
+    public static void main(String[] args){
+        //Initialize
+        int position;
+
+        //Scanner onject
+        Scanner keyboard = new Scanner(System.in);
+
+        //Request
+        System.out.print("Enter a number. ");
+        position = keyboard.nextInt();
+
+        //Switch
+        switch(position){
+            case 1:
+                System.out.println("You won!");
+                break;
+            case 2:
+            case 3:
+                System.out.println("You were so close!");
+                break;
+            default:
+                System.out.println("Time to train harder!");
+        }
+    }
+}
---
+++ ShortbreadPrices.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ShortbreadPrices{
+    public static void main(String[] args){
+        //Initialize counts
+        //Each shortbread = 5$
+        //Once it reaches a certain threshold, then apply that certain discount
+        int packageCount;
+        double discount1 = 0.80, discount2 = 0.70, discount3 = 0.60, discount4 = 0.50;
+        double price;
+
+        //Scanner object
+        Scanner keyboard = new Scanner(System.in);
+
+        //Ask for how many packages they want and then multiply by 5 for base price
+        System.out.print("How many packages of shortbread do you want to order? ");
+        packageCount = keyboard.nextInt();
+        price = packageCount*5;
+
+        //Result
+        if(packageCount >= 10 && packageCount <= 19){
+            price*=discount1;
+            System.out.printf("For your order of " + packageCount + " 1shortbread, you will be getting a dicount of 20 percent. Your total is $%.2f", price);
+        }
+        else if(packageCount >= 20 && packageCount <= 49){
+            price*=discount2;
+            System.out.printf("For your order of " + packageCount + " shortbread, you will be getting a dicount of 30 percent. Your total is $%.2f", price);
+        }
+        else if(packageCount >= 50 && packageCount <= 99){
+            price*=discount3;
+            System.out.printf("For your order of " + packageCount + " shortbread, you will be getting a dicount of 40 percent. Your total is $%.2f", price);
+        }
+        else if(packageCount >= 100){
+            price*=discount4;
+            System.out.printf("For your order of " + packageCount + " shortbread, you will be getting a dicount of 50 percent. Your total is $%.2f", price);
+        }
+        else{
+            System.out.printf("For your order of " + packageCount + " shortbread, you will be getting no discount. Your total is $%.2f", price);
+        }
+    }
+}
